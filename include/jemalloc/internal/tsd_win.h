6e7b602372127ab9e49f7dccc91d36e8ad1b126b
Ben Niu <beniu@microsoft.com>
2024-02-17 20:37:25 -0800

    Fix ifdef

--- include/jemalloc/internal/tsd_win.h
+++ include/jemalloc/internal/tsd_win.h
@@ -90,8 +90,8 @@ tsd_boot0(void) {
 	}
 	_malloc_tsd_cleanup_register(&tsd_cleanup_wrapper);
 	tsd_wrapper_set(&tsd_boot_wrapper);
-	tgv2_mod = LoadLibraryA("api-ms-win-core-processthreads-l1-1-8.dll");
 #ifdef JEMALLOC_WIN32_TLSGETVALUE2
+	tgv2_mod = LoadLibraryA("api-ms-win-core-processthreads-l1-1-8.dll");
 	if (tgv2_mod != NULL) {
 		tlsgetvalue2 = (TGV2)GetProcAddress(tgv2_mod, "TlsGetValue2");
 	}
